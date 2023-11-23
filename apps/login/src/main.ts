e613281e05c4eb8213d8ea3410979fe9432e8bd6
dhipgraby <keneth.zambrano4@gmail.com>
2023-11-23 18:29:45 +0100

    swagger & users structure

--- apps/login/src/main.ts
+++ apps/login/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { LoginModule } from './login.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(LoginModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Users API')
+    .setDescription('API for handling users')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
+
+
   await app.listen(3000);
 }
 bootstrap();
